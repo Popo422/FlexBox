940ba35dd69ee45e068074036e085aae2fb5428a
Zernie Reyes <zerniereyes@gmail.com>
2023-05-29 22:08:41 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="flexbox-item flexbox-1"></div>
+        <div className="flexbox-item flexbox-2"></div>
+        <div className="flexbox-item flexbox-3"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
